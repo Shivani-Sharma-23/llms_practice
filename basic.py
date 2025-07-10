fb8124dff990a2a84d86db8dff0926d56157b9c4
Shivani Sharma <112687652+Shivani-Sharma-23@users.noreply.github.com>
2025-07-10 23:00:19 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,10 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+llm = OpenAI(temperature=0.7)
+prompt = PromptTemplate(input_variables=["topic"], template="Explain {topic} in simple terms.")
+chain = LLMChain(llm=llm, prompt=prompt)
+
+response = chain.run("quantum computing")
+print(response)
